a055b4edd1044b63a2b12c9ce947d89146b4338a
tommy3wood <thomaswcurran1014@gmail.com>
2020-04-09 19:03:33 -0500

    setup app component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>   
+        <input type="checkbox"/>
+        <p>Sweep floors</p> 
+        <input type="checkbox"/>
+        <p>Clean gutters</p> 
+        <input type="checkbox"/>
+        <p>Rob the federal reserve blind</p>       
+    </div>
+  )
+}
+
+export default App
